55bf78ab4c92a9e089e4a5dd773830100ad2bec7
bodrovis <bodrovis@protonmail.com>
2021-06-11 17:29:56 +0300

    lesson 3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,47 @@
 class QuestionsController < ApplicationController
+  def show
+    @question = Question.find_by id: params[:id]
+  end
+
+  def destroy
+    @question = Question.find_by id: params[:id]
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  def edit
+    @question = Question.find_by id: params[:id]
+  end
+
+  def update
+    @question = Question.find_by id: params[:id]
+    if @question.update question_params
+      redirect_to questions_path
+    else
+      render :edit
+    end
+  end
+
   def index
     @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new question_params
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/questions', to: 'questions#index'
+  resources :questions
 
   root 'pages#index'
 end
