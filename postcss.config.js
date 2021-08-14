3347c55e3962739aa0d36ab4899bc687f0545286
bodrovis <bodrovis@protonmail.com>
2021-08-14 22:08:08 +0300

    bump

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
